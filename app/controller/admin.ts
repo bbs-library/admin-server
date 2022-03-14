4f0eb845c460476f916e5e71b3576350a46fe26d
lixin <lixin@zhongbaounion.com>
2022-03-14 20:02:24 +0800

    admin接口实现

--- app/controller/admin.ts
+++ app/controller/admin.ts
@@ -1,24 +1,37 @@
 import { Controller } from 'egg';
-import { Prefix, Get, Query, Post, Body } from '@lima/egg-aop-decorator';
+import { Prefix, Get, Query, Post, Body, Param, Nullable } from '@lima/egg-aop-decorator';
 
 @Prefix('/api/admin')
 export default class AdminController extends Controller {
-    @Get('/getList')
-    public async getList() {
-        this.ctx.body = await this.service.admin.getAdminList();
+
+    // 登陆
+    @Post('/login')
+    public async adminLogin(@Body('admin') admin: string, @Body('password') password: string) {
+        this.ctx.body = await this.service.admin.login({ admin, password });
     }
 
-    @Get('/getDetailById')
-    public async getDetailById(@Query('id') id: number) {
-        this.ctx.body = await this.service.admin.getAdminById(id);
-    }
-
-    @Post('/createOne')
-    public async createOne(@Body('power') power: number,
+    // 新增管理员
+    @Post('/')
+    public async create(@Body('power') power: number = 1,
         @Body('admin') admin: string,
         @Body('password') password: string,
     ) {
-        const data = await this.service.admin.createAdmin(admin, password, power);
-        this.ctx.body = data;
+        this.ctx.body = await this.service.admin.createAdmin(admin, password, power);
     }
+
+    // 查询所有管理员
+    @Get('/')
+    public async list(@Query('page') page: number = 1,
+        @Query('pageSize') pageSize: number = 10,
+        @Query('name') @Nullable name?: string,
+    ) {
+        this.ctx.body = await this.service.admin.getAdminList(page, pageSize, name);
+    }
+
+    // 通过id查询详情
+    @Get('/:id')
+    public async getDetailById(@Param('id') id: number) {
+        this.ctx.body = await this.service.admin.getAdminById(id);
+    }
+
 }
--- app/service/admin.ts
+++ app/service/admin.ts
@@ -3,19 +3,81 @@ import { Service } from 'egg';
 export default class AdminService extends Service {
     private adminModel = this.ctx.model.Admin;
 
+    // 登陆
+    public async login({ admin, password }) {
+        const data = await this.adminModel.findOne({ where: { admin, password } });
+        if (data?.id) {
+            delete data.password;
+            return {
+                data,
+                status: 200,
+                message: '登陆成功',
+            };
+        }
+        return {
+            status: 404,
+            message: '没有查询到该用户的相关信息',
+        };
+    }
+
+    // 新增管理员
+    public async createAdmin(admin: string, password: string, power: number) {
+        const result = await this.adminModel.findOne({ where: { admin } });
+        this.ctx.logger.info('result', result);
+        if (result) {
+            return {
+                status: 400,
+                message: '该用户已注册',
+            };
+        }
+        await this.adminModel.findOrCreate({
+            where: { admin },
+            defaults: { admin, password, power, createAt: new Date() },
+        });
+
+        return {
+            status: 200,
+            message: '操作成功',
+        };
+    }
+
     // 获取admin管理员列表
-    public getAdminList() {
-        return this.adminModel.findAll();
+    public async getAdminList(page: number, pageSize: number, admin?: string) {
+        const query = {
+            limit: pageSize,
+            offset: pageSize * (page - 1),
+        };
+        const data = await this.adminModel.findAndCountAll(admin
+            ? {
+                where: { admin },
+                ...query,
+            }
+            : query,
+        );
+
+        return {
+            status: 200,
+            message: '查询成功',
+            data,
+        };
     }
 
     // 通过id查询管理员
-    public getAdminById(id: number) {
-        return this.ctx.model.Admin.findOne({ where: { id } });
+    public async getAdminById(id: number) {
+        const data = await this.adminModel.findOne({ where: { id } });
+        if (data) {
+            return {
+                status: 200,
+                message: '查询成功',
+                data,
+            };
+        }
+        return {
+            status: 404,
+            message: '没有查询到此账户的相关信息',
+        };
     }
 
     // 创建一个管理员
-    public createAdmin(admin: string, password: string, power: number) {
-        return this.ctx.model.Admin.create({ admin, password, power, createAt: new Date() });
-    }
 
 }
