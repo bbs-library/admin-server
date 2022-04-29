b2b1ae1ecdb2b8d9db4783a88362d5860f20ac49
lixin <lixin@zhongbaounion.com>
2022-04-29 17:41:11 +0800

    fix: 🚀 修改local config文件

--- config/config.local.ts
+++ config/config.local.ts
@@ -1,4 +1,5 @@
 import { REDIS_HOST, REDIS_PWD } from './redis.config';
+import { MYSQL_DATABASE, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USERNAME } from './mysql.config';
 
 exports.redis = {
     client: {
@@ -9,13 +10,25 @@ exports.redis = {
     },
 };
 
+// exports.sequelize = {
+//     dialect: 'mysql',
+//     database: 'bbs',
+//     host: 'localhost',
+//     port: 3306,
+//     username: 'root',
+//     password: 'lixin19940802',
+//     define: {
+//         underscored: false,
+//     },
+// };
+
 exports.sequelize = {
     dialect: 'mysql',
-    database: 'bbs',
-    host: 'localhost',
+    database: MYSQL_DATABASE,
+    host: MYSQL_HOST,
     port: 3306,
-    username: 'root',
-    password: 'lixin19940802',
+    username: MYSQL_USERNAME,
+    password: MYSQL_PASSWORD,
     define: {
         underscored: false,
     },
